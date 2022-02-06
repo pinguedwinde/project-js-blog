a892af17a37b049254d1fcca28f5ab0a0ef78448
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2022-02-06 14:58:09 +0100

    Part-1 : Created the first part of the project included 2 pages (MPA) for viewing and creating articles

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const articles = [
+  {
+    author: "AmusedWeather3114",
+    img: "https://randomuser.me/api/portraits/women/16.jpg",
+    category: "Researches",
+    title: "List of Free Kindle e-books at Amazon",
+    content:
+      "Inventions, Researches and Writings of Nikola Tesla: Including Tesla's Autobiographyhttps://www.amazon.com/dp/B08LZLCQ89In Dog We Trust (Cozy Dog Mystery): #1 in the Golden Retriever Mystery Series (… [+1782 chars]. Inventions, Researches and Writings of Nikola Tesla: Including Tesla's Autobiographyhttps://www.amazon.com/dp/B08LZLCQ89In Dog We Trust (Cozy Dog Mystery): #1 in the Golden Retriever Mystery Series (… [+1782 chars]\n\nInventions, Researches and Writings of Nikola Tesla: Including Tesla's Autobiographyhttps://www.amazon.com/dp/B08LZLCQ89In Dog We Trust (Cozy Dog Mystery): #1 in the Golden Retriever Mystery Series (… [+1782 chars]\n\nInventions, Researches and Writings of Nikola Tesla: Including Tesla's Autobiographyhttps://www.amazon.com/dp/B08LZLCQ89In Dog We Trust (Cozy Dog Mystery): #1 in the Golden Retriever Mystery Series (… [+1782 chars]. Inventions, Researches and Writings of Nikola Tesla: Including Tesla's Autobiographyhttps://www.amazon.com/dp/B08LZLCQ89In Dog We Trust (Cozy Dog Mystery): #1 in the Golden Retriever Mystery Series (… [+1782 chars]",
+    createdAt: "2022-02-05T16:07:40.215Z",
+    _id: "61fea0ccbc3f94ed58533a0d",
+  },
+  {
+    author: "Roadshow staff",
+    img: "https://randomuser.me/api/portraits/women/22.jpg",
+    category: "Electrical Cars",
+    title: "Best electric cars and EVs for 2022 - Roadshow",
+    content:
+      "If you're ready to shop electric cars, you're in the right place. Our editors have driven nearly every new EV that's currently on sale, and we'd love to help steer you in the right direction.The investment of $10 billion to $20 billion will be spread out over the next five to ten years and will include converting its present factories around the world to electric-vehicle production, the report said, citing people familiar with the plan.\n\nFord Motor Co is planning additional investment of up to $20 billion in building its electric vehicles, Bloomberg News reported on Tuesday. The investment of $10 billion to $20 billion will be spread",
+    createdAt: "2022-02-05T16:10:05.174Z",
+    _id: "61fea15dbc3f94ed58533a0e",
+  },
+  {
+    author: "Michael Foster",
+    img: "https://randomuser.me/api/portraits/men/5.jpg",
+    category: "Health",
+    title: "CDC director backs Moderna COVID-19 vaccine shots - Fox News",
+    content:
+      "U.S. Centers for Disease Control and Prevention (CDC) Director Dr. Rochelle Walensky said Friday that she endorsed an agency advisory committee's recommendation for the use of Moderna's COVID-19 vaccine in individuals 18 years of age and older.\n\nU.S. Centers for Disease Control and Prevention (CDC) Director Dr. Rochelle Walensky said Friday that she endorsed an agency advisory committee's recommendation for the use of Moderna's COVID-19. Parents who thought their children under 5 would have to wait several more months for COVID-19 vaccinations instead just might get them as early as March. Here’s what needs to happen first, and some things to expect if they do become available in the U.S.\n\nParents who thought their children under 5 would have to wait several more months for COVID-19vaccinations instead just might get them as early as March. Here’s what needs to happen first, and some t… [+3096 chars].\n",
+    createdAt: "2022-02-05T16:14:01.838Z",
+    _id: "61fea249bc3f94ed58533a0f",
+  },
+];
